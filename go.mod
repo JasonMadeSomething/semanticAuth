a489a0bb7b255a2428ffe243853ab3856588aa7b
Jason Teitelman <jasonmadesomething@gmail.com>
2025-07-25 20:20:04 -0400

    Add server code

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module semantic-auth
 go 1.24.5
 
 require (
+	github.com/go-chi/chi/v5 v5.2.2 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/klauspost/compress v1.16.7 // indirect
--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"log"
+	"net/http"
 
 	"semantic-auth/db"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/joho/godotenv"
 )
 
@@ -15,5 +18,14 @@ func main() {
 	}
 
 	db.Connect()
-	// Your logic here: routes, inserts, etc.
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("OK"))
+	})
+
+	log.Println("Listening on :8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
